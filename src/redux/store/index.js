6613a5280e69a83848702a0809b65bb8d4292190
pitaz <peter.odekwo@gmail.com>
2019-12-10 17:59:58 +0100

    code challenge

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,11 +1,11 @@
 import {createStore, applyMiddleware} from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+// import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 import reducer from '../reducers';
 
 const store = createStore(
 	reducer, 
-	composeWithDevTools(applyMiddleware(thunk))
+	applyMiddleware(thunk)
 );
 
 export default store;
